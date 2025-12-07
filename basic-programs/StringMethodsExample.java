185a9b34b260df09bbaedc831e2c9d3f61a21458
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 12:27:21 +0400

    Add StringMethodsExample.java for string operations

---
+++ basic-programs/StringMethodsExample.java
@@ -0,0 +1,14 @@
+public class StringMethods
+{
+public static void main (String [] args)
+{
+String sentence;
+String str1, str2, str3;
+int index; 
+sentence = "Now is the time for the birthday party";
+System.out.println("sentence = \"" + sentence + "\"");
+System.out.println("the length of the sentence = " + sentence.length());
+System.out.println("The character at index 16 in" + " sentence = " + sentence.charAt(16));
+System.out.println("the index of the first t in" + "sentence = " + sentence.indexOf('t'));
+}
+}
