64cb1463152fc3d8308deafd78b61bde3c44f384
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 11:46:52 +0400

    Add PrintDigits.java to print digits of a number

---
+++ basic-programs/PrintDigits.java
@@ -0,0 +1,11 @@
+public class tut2_ex2
+{
+   public static void main(String[] args)
+   {
+int num = 3245;
+System.out.println("3");
+System.out.println("2");
+System.out.println("4");
+System.out.println("5");
+}
+}
