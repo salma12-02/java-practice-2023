59bcfd37a9dc7d51c7372e33100a6fda7eef2f97
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 15:59:47 +0400

    Create CircleAreaCalculator.java

---
+++ input-processing/CircleAreaCalculator.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Lap2_pb2
+{
+public static void main(String[] args)
+{
+Scanner console = new Scanner (System.in);
+
+final double pi = 3.14;
+
+double radius, area;
+
+System.out.println("Enter the radius");
+
+radius = console.nextDouble();
+
+area = 3.14 * radius * radius;
+
+area = Math.PI*radius * radius;
+
+System.out.println ("The area is :" + area);
+}
+}
+
