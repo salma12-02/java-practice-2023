9825c4c79264c8258bda4bccf4ad94c34ae4027e
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 12:17:10 +0400

    Add Numbers1to20.java to print numbers from 1 to 20

---
+++ basic-programs/Numbers1to20.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class oneTotwinty {
+public static void main(String [] args) {
+Scanner console = new Scanner(System.in);
+int num = 1;
+System.out.println("The numbers from 1 to 20: ");
+while (num <= 20) {
+System.out.println(num);
+num++; }
+}
+}
