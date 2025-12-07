dac9c0367d921a6a1579141c260a8fa25905c8db
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 16:51:53 +0400

    Create ArrayReverser.java

---
+++ basic-programs/ArrayReverser.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayRevers_8
+{ 
+ public static void main(String [] args)
+ {
+  char [] ch = {'A', 'B', 'C', 'D', 'E', 'F'};
+  for (int i = ch.length - 1; i >= 0; i--) 
+  {
+   System.out.print(ch[i] + " ");
+  }
+ 
+ }
+ 
+}
