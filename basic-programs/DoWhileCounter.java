ee568e3b9c4de58d514dbd40ecb18115d4f47594
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 12:01:04 +0400

    Add DoWhileCounter class to demonstrate do-while loop

---
+++ basic-programs/DoWhileCounter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class For_DoWhile
+{
+ public static void main(String [] args)
+ {
+  Scanner console = new Scanner (System.in);
+  int number = 0;
+  do
+  {
+   number++;
+   System.out.print(number + " ");
+  }
+  while (number < 10);
+//   for(number = 1; number <= 10; number++)
+//    System.out.print(number + " ");
+//    System.out.println();
+    
+ }
+
+}
