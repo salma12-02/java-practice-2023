851c0d06379f1209eeacae13f9f6a1bcd46a7f42
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 11:37:58 +0400

    Add ArithmeticExpressions.java for basic calculations

---
+++ basic-programs/ArithmeticExpressions.java
@@ -0,0 +1,14 @@
+public class tut2_ex1
+{
+   public static void main(String[] args)
+   {
+      System.out.println("25 / 3 = "+(25/3));
+      System.out.println("20 - 12 / 4 * 2 = "+(20 - 12 / 4 * 2));
+      System.out.println("32 % 7 = "+(32 % 7));
+      System.out.println("3 - 5 % 7 = "+(3 - 5 % 7));
+      System.out.println("18.0 / 4 = "+(18.0 / 4));
+      System.out.println("28 - 5 / 2.0 = "+(28 - 5 / 2.0));
+      System.out.println("17 + 5 % 2 - 3 = "+(17 + 5 % 2 - 3));
+      System.out.println("15.0 + 3.0 *2.0 / 5.0 = "+(15.0 + 3.0 *2.0 / 5.0));
+}
+}
