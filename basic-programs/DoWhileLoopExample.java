91677084bf1f69ea66081ead6b38750f85e085a8
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 11:39:28 +0400

    Add DoWhileLoopExample.java with basic loop logic

---
+++ basic-programs/DoWhileLoopExample.java
@@ -0,0 +1,14 @@
+public class Tut10
+{
+  public static void main(String[] args)
+  {
+        // 5 times x=55 y=50
+    int x=5, y=50;
+    do
+    {
+      x = x+10;
+      System.out.println(x);
+    }
+    while(x<y);
+    }
+    }
