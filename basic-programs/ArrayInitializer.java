0665df0901646684c6721fa23d4b7dbc3e94aa89
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 17:03:28 +0400

    Create ArrayInitializer.java

---
+++ basic-programs/ArrayInitializer.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*; 
+public class Array1 {
+public static void main(String [] args) {
+Scanner console = new Scanner(System.in);
+int [] AR = new int [3];
+AR [1] = 30;
+AR [2] = 20;
+AR [0] = AR [1] - AR [2];
+System.out.println(AR [0]); }    }
