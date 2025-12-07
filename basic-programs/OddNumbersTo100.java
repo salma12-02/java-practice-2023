4f3e965cd839e683310b87fb2fe86af5f3015fc1
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 13:37:45 +0400

    Add program to print odd numbers up to 100

---
+++ basic-programs/OddNumbersTo100.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ch5p2practice
+{
+ public static void main(String [] args)
+ { 
+  Scanner console = new Scanner(System.in);
+  int num;
+  for (num = 1; num < 100; num++)
+  {
+   if (num % 2 == 1)
+   System.out.println(num + " ");
+  } 
+}
+}
