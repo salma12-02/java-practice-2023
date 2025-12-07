81202c7331e46f04661b157780b675c9bbb76760
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 16:01:22 +0400

    Create SimpleDivision.java

---
+++ input-processing/SimpleDivision.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Lap2_pb4
+{
+public static void main(String[] args)
+{
+Scanner console = new Scanner (System.in);
+System.out.println("Enter Y & X value");
+
+int y;
+y = console.nextInt();
+int x;
+x = console.nextInt();
+System.out.println("y / x =" + (y / x));
+}
+}
