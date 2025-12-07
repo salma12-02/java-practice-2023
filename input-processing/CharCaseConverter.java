3a409a4dd13742b6065a3ed6421fd0717cb843e3
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 13:59:48 +0400

    Implement CharCaseConverter for character case conversion

---
+++ input-processing/CharCaseConverter.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Math.*;
+import static java.lang.Character.*;
+public class Ch8P1Training 
+{ 
+ public static void main(String [] args)
+ {
+  Scanner console = new Scanner(System.in);
+  //Class Math Problems:
+  
+  //ABS(X):
+  //int num;
+  //System.out.println("Enter a number: "); 
+  //num = console.nextInt();
+  //System.out.printf("The absolute value of %d is = %d", num, abs(num));
+ 
+ //CEIL(X):
+ //double num;
+ //System.out.println("Enter a decimal number: "); 
+ //num = console.nextDouble();
+ //System.out.printf("The largest closest whole number to %.1f is = %.1f", num, ceil(num));
+ 
+ //EXP(X):
+ //double num;
+ //System.out.println("Enter a number: "); 
+ //num = console.nextDouble();
+ //System.out.printf("The value of the natural number e raised to the power %.1f = %.1f", num, exp(num));
+
+//FLOOR(X): 
+ //double num;
+ //System.out.println("Enter a dicemal number: ");
+ //num = console.nextDouble();
+ //System.out.printf("The smallest closest whole number to %.1f is %.1f", num, floor(num));
+ 
+ //LOG(X):
+ //double num;
+ //System.out.println("Enter a number: ");
+ //num = console.nextDouble();
+ //System.out.printf("The natural log of %.1f is %.1f", num, log(num));
+ 
+ //LOG10(X):
+ //double num;
+ //System.out.println("Enter a number: ");
+ //num = console.nextDouble();
+ //System.out.printf("The log base-10 of %.1f is = %.1f", num, log10(num)); 
+ 
+ //MAX(X, Y):
+ //int x, y;
+ //System.out.println("Enter 2 numbers: ");
+ //x = console.nextInt();
+ //y = console.nextInt();
+ //System.out.printf("The maximum value between %d and %d is %d", x, y, max(x, y));
+ 
+ //MIN(X, Y):
+ //int n1, n2;
+ //System.out.println("Enter 2 numbers: ");
+ //n1 = console.nextInt();
+ //n2 = console.nextInt();
+ //System.out.printf("The minimum value between %d and %d is %d", n1, n2, min(n1, n2));
+ 
+ //POW(X, Y):
+ //double u1, u2;
+ //System.out.println("Enter 2 numbers, the second one will be raised to the power of the first one: ");
+ //u1 = console.nextDouble();
+ //u2 = console.nextDouble();
+ //System.out.printf("%.1f raised to the power of %.1f is = %.1f", u1, u2, pow(u1, u2));
+ 
+ //ROUND(X): 
+ //double num;
+ //System.out.println("Enter a decimal number: ");
+ //num = console.nextDouble();
+ //System.out.printf("%.1f rounded up = %d", num, round(num));
+ 
+ //SQRT(X): 
+ //double number;
+ //System.out.println("Enter a number: ");
+ //number = console.nextDouble();
+ //System.out.printf("The square root of %.1f is = %.1f", number, sqrt(number));
+ 
+ //COS(X): 
+ //double n;
+ //System.out.println("Enter a number: ");
+ //n = console.nextDouble();
+ //System.out.printf("The cosine of %.1f is = %.1f", n, cos(n));
+ 
+ //SIN(X): 
+ //double n;
+ //System.out.println("Enter a number: ");
+ //n = console.nextDouble();
+ //System.out.printf("The sine of %.1f is = %.1f", n, sin(n));
+
+//TAN(X):
+//double n;
+ //System.out.println("Enter a number: ");
+ //n = console.nextDouble();
+ //System.out.printf("The tangent of %.1f is = %.1f", n, tan(n));
+
+//ISDIGIT(CH):
+//char ch;
+//System.out.println("Enter a character digit: ");
+//ch = console.next().charAt(0);
+//System.out.print(isDigit(ch));
+
+//ISLETTER(CH):
+//char ch;
+//System.out.println("Enter a character: ");
+//ch = console.next().charAt(0);
+//System.out.print(isLetter(ch));
+
+//ISLOWERCASE(CH):
+//char ch;
+//System.out.println("Enter a character: ");
+//ch = console.next().charAt(0);
+//System.out.print(isLowerCase(ch));
+
+//ISUPPERCASE(CH):
+//char ch;
+//System.out.println("Enter a character: ");
+//ch = console.next().charAt(0);
+//System.out.print(isUpperCase(ch));
+
+//TOLOWERCASE(CH):
+//char ch;
+//System.out.println("Enter a character: ");
+//ch = console.next().charAt(0);
+//System.out.print(toLowerCase(ch));
+
+//TOUPPERCASE(CH):
+char ch;
+System.out.println("Enter a character: ");
+ch = console.next().charAt(0);
+System.out.print(toUpperCase(ch));
+
+ }
+
+}
+
+
+
+
