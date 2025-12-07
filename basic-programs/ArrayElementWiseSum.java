14a194adcdefe30c380a364a8154e4767b8eda17
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 16:27:08 +0400

    Create ArrayElementWiseSum.java

---
+++ basic-programs/ArrayElementWiseSum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArraySumS9 
+{ 
+ public static void main(String [] args)
+ {
+  int [] A = {56, 79, 20, 42};
+  int [] B = {62, 10, 78, 62};
+  int sum0 = A[0] + B[0];
+  int sum1 = A[1] + B[1];
+  int sum2 = A[2] + B[2];
+  int sum3 = A[3] + B[3];
+  System.out.println("The sum of each element in both arrays, 1st = "+sum0+" 2nd = "+sum1+" 3rd = "+sum2+" 4th = "+sum3);
+  }
+ }
