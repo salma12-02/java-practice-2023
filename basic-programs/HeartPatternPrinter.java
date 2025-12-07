b84b62edf395d3c6849ce4a78781ed91705b434c
salma12-02 <salma.03.khalid@gmail.com>
2025-12-07 13:46:21 +0400

    Create HeartPatternPrinter.java for heart pattern
    Add a Java program to print a heart pattern using characters.

---
+++ basic-programs/HeartPatternPrinter.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ch6heart
+{
+ public static void main(String [] args)
+ {
+ Scanner console = new Scanner(System.in);
+ char ch = 'S';
+  System.out.printf("%35c", ch);
+  System.out.printf("%20c\n", ch);
+  System.out.printf("%45c\n\n", ch);
+  System.out.printf("%35c", ch);
+  System.out.printf("%20c\n\n", ch);
+  System.out.printf("%40c", ch);
+  System.out.printf("%10c\n", ch);
+  System.out.printf("%45c\n", ch);
+
+
+
+
+
+ }
+
+}
